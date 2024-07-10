462ae7cb9aef4dee4a6c24415f8fceef08a10343
Abiud Sokomi <117529572+sokomi9@users.noreply.github.com>
2024-07-10 04:11:14 +0300

    Add files via upload
    added a delete button to details.php

--- details.php
+++ details.php
@@ -1,7 +1,18 @@
 <?php 
+include('templates/navbar.php'); 
 include('config/dbconn.php');
-include('templates/navbar.php');
-include('templates/header.php');
+
+if (isset($_POST['delete'])) {
+   $id_to_delete = mysqli_real_escape_string($con, $_POST['id_to_delete']);
+
+   $sql = "DELETE FROM table1 WHERE id=$id_to_delete";
+
+   if (mysqli_query($con, $sql)) {
+      header("Location: index.php");
+   }{
+      echo "query errror: " . mysqli_error($con);
+   }
+}
 
 if (isset($_GET['id'])) {
    $id = mysqli_real_escape_string($con, $_GET['id']);
@@ -10,10 +21,16 @@ if (isset($_GET['id'])) {
    $entry = mysqli_fetch_assoc($result);
    mysqli_free_result($result);
    mysqli_close($con);
-   print_r($entry);
+   //print_r($entry);
 }
-
 ?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include('templates/header.php'); ?>
 
    <div class="container center">
       <?php if ($entry): ?>
@@ -22,10 +39,16 @@ if (isset($_GET['id'])) {
          <p><?php echo date($entry['entry_time']); ?></p>
          <h5>Items:</h5>
          <p><?php echo htmlspecialchars($entry['items']); ?></p>
+      <!-- DELETE FORM -->
+      <form action="details.php" method="POST">
+      <input type="hidden" name="id_to_delete" value="<?php echo $entry['id']; ?>"><br />
+      <input type="submit" name="delete" value="Delete" class="btn brand z-depth-0">
+      </form>
       <?php else: ?>
           <p>No such child came in!</p>
       <?php endif; ?>   
    </div>
-
+  
 <?php include('templates/footer.php'); ?>
 
+</html>
